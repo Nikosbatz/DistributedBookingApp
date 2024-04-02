5cb0f74664196a0fdb3f0c6b05a457865ec223f7
giwrgosisf <164655527+giwrgosisf@users.noreply.github.com>
2024-04-02 19:01:15 +0300

    Add files via upload

---
+++ src/Server/MasterThread.java
@@ -0,0 +1,173 @@
+package Server;
+
+import Entities.MessageData;
+import Worker.*;
+import java.awt.print.PrinterException;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+public class MasterThread implements Runnable{
+    private Socket client;
+    private  final ArrayList<Worker> workerslist;
+
+
+    MasterThread(Socket client, ArrayList<Worker> workersList){
+        this.client = client;
+        this.workerslist = workersList;
+    }
+
+    @Override
+    public void run(){
+
+        try {
+            PrintWriter out = new PrintWriter(client.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            ObjectInputStream objectIn = new ObjectInputStream(client.getInputStream());
+
+
+
+            out.println("Hello are you a renter or a manager?");
+            out.println("press (a) for manager OR (b) for renter");
+            out.flush();
+
+            // read client response
+            String line = ((MessageData)objectIn.readObject()).data;
+            System.out.println(line);
+
+            while (line != null){
+
+                if (line.equals("a")){
+                    // Manager interface method call
+                    runManagerInterface(out, in, objectIn);
+                    break;
+                }
+                else{
+                    // renter interface method call
+                    runRenterInterface(out, in);
+                    break;
+                }
+                //line = in.readLine();
+            }
+        }
+        catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void runManagerInterface(PrintWriter out, BufferedReader in, ObjectInputStream objectIn){
+
+        out.println("Welcome to manager interface, would you like to insert a new room?");
+        out.flush();
+
+        try {
+            String response = ((MessageData)objectIn.readObject()).data;
+            if (response.equals("y")) {
+
+                // The method that Master will request from Workers
+                String methodRequest = "insert";
+
+                // Asking client to insert .json file
+                out.println("Insert json : ");
+                out.flush();
+
+                // Reads .json data from Client.
+                MessageData message = (MessageData) objectIn.readObject();
+
+                // Establishes connection with Workers
+                ArrayList<Socket> sockets = connectWithWorkers();
+
+                // Instantiate JSON parser
+                JSONParser parser = new JSONParser();
+                JSONObject json = (JSONObject) parser.parse(message.data);
+
+                // Choosing Worker Node based on hashCode
+                long hashCode = (long) json.get("roomName").hashCode();
+                int nodeId =(int) hashCode % workerslist.size();
+                //System.out.println(nodeId);
+
+
+
+                for (Socket socket : sockets) {
+                    ObjectOutputStream objectOut = new ObjectOutputStream(socket.getOutputStream());
+                    // Sends to the worker the method that client requests
+                    objectOut.writeObject(methodRequest);
+                    objectOut.writeObject(message);
+                }
+
+            }
+
+        }
+        catch (IOException | ClassNotFoundException | ParseException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void runRenterInterface(PrintWriter out, BufferedReader in){
+        MessageData type = (MessageData) "renter";
+        out.println("Welcome to renter interface...");
+        out.flush();
+        out.println("Choose one of the following options.");
+        out.flush();
+        out.println("1. Filter the rooms. \n2. Make a reservation \n3. Rate a room");
+        out.flush();
+        try {
+            String response = ((MessageData)objectIn.readObject()).data;
+            if (response.equals("1")) {
+
+                // The method that Master will request from Workers
+                String methodRequest = "filter";
+
+                // Asking client to insert .json file
+                out.println("Choose filters : ");
+                out.flush();
+
+                // Reads filter data from Client.
+                List<String> filters = (List<String>) objectIn.readObject();
+
+                // Establishes connection with Workers
+                ArrayList<Socket> sockets = connectWithWorkers();
+
+
+                for (Socket socket : sockets) {
+                    ObjectOutputStream objectOut = new ObjectOutputStream(socket.getOutputStream());
+                    // Sends to the worker the method that client requests
+                    objectOut.writeObject(type);
+                    objectOut.writeObject(methodRequest);
+                    objectOut.writeObject(filters);
+                }
+
+            }
+
+        }
+        catch (IOException | ClassNotFoundException | ParseException e){
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Socket> connectWithWorkers(){
+
+        // List of all the connections with Workers
+        ArrayList<Socket> sockets = new ArrayList<Socket>();
+
+        try {
+            for (Worker w : workerslist) {
+
+                // Establish a new connection with each of the Workers
+                sockets.add(new Socket("localhost", w.getPort()));
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        return sockets;
+    }
+
+
+}
