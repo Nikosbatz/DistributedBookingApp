380b1d61b91fa3952535a0c3526ece78809e7525
giwrgosisf <164655527+giwrgosisf@users.noreply.github.com>
2024-04-15 01:49:26 +0300

    Add files via upload

---
+++ src/Entities/AccommodationRoom.java
@@ -0,0 +1,197 @@
+package Entities;
+import org.json.simple.*;
+
+import java.io.Serializable;
+import java.util.AbstractSet;
+import java.util.HashMap;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+public class AccommodationRoom implements Serializable {
+
+    private String name;
+    private  int capacity;
+    private int price;
+    private String area;
+    private int stars;
+    private int noOfReviews;
+    private int sumOfReviews;
+    private String imagePath;
+    private int owner;
+
+    private HashMap<LocalDate,LocalDate> availableDates = new HashMap<LocalDate,LocalDate>();
+    private HashMap<LocalDate,LocalDate> bookedDates = new HashMap<LocalDate,LocalDate>();
+
+    // Create a new instance using a JSONObject object.
+    public AccommodationRoom(JSONObject json){
+        setArea((String)json.get("area"));
+        setCapacity( (long) json.get("capacity"));
+        setName((String) json.get("roomName"));
+        setStars((long)json.get("stars"));
+        setNoOfReviews((long)json.get("noOfReviews"));
+        setSumOfReviews(getNoOfReviews() * getStars());
+        setPrice((long) json.get("price"));
+        setImagePath((String)json.get("imagePath"));
+        setAvailableDates(json);
+    }
+
+
+
+    public void addReview(int review){
+
+        setSumOfReviews(getSumOfReviews() + review);
+        setNoOfReviews(getNoOfReviews()+1);
+        setStars(getSumOfReviews()/getNoOfReviews());
+    }
+
+
+    public String toString(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        String str = "\nName: " + getName() +"\nArea: " + getArea() + "\nCapacity: " + getCapacity() +
+                "\nStars: " + getStars() + "\nPrice: " + getPrice() + "\nAvailable Dates: ";
+
+        for (LocalDate tempDate: availableDates.keySet()){
+            String date1 = tempDate.format(formatter);
+            String date2 = availableDates.get(tempDate).format(formatter);
+            str += "\n" + date1 + " -> " + date2 ;
+        }
+
+        str += "\n";
+
+        return (str);
+    }
+
+    // Just Getters and Setters below
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public int getNoOfReviews() {
+        return noOfReviews;
+    }
+
+    public void setNoOfReviews(long noOfReviews) {this.noOfReviews = (int) noOfReviews;}
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(long capacity) {
+        this.capacity = (int) capacity;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(long stars) {
+        this.stars = (int)stars;
+    }
+    public int getSumOfReviews() {return sumOfReviews;}
+
+    public void setSumOfReviews(int sumOfReviews) {this.sumOfReviews = sumOfReviews;}
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public int getPrice() {return price;}
+
+    public void setPrice(long price) {this.price = (int)price;}
+
+    public int getOwner() {
+        return owner;
+    }
+
+    public void setOwner(int owner) {
+        this.owner = owner;
+    }
+
+    public HashMap<LocalDate, LocalDate> getAvailableDates() {       //!!!!!!!!!!
+        return availableDates;
+    }
+
+    public void setAvailableDates(JSONObject json) {
+        JSONArray availableDatesArray = (JSONArray) json.get("availableDates");
+        for (Object date : availableDatesArray) {
+            JSONObject availDate = (JSONObject) date;
+            String dateFirst = (String) availDate.get("dateFirst");
+            String dateLast = (String)  availDate.get("dateLast");
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            LocalDate dateStart = LocalDate.parse(dateFirst, formatter);
+            LocalDate dateEnd = LocalDate.parse(dateLast, formatter);
+
+            synchronized (availableDates) {
+                availableDates.put(dateStart, dateEnd);
+            }
+        }
+    }
+
+    // Override setAvailableDates()
+    public void setAvailableDates(LocalDate dateStart, LocalDate dateEnd){
+        HashMap<LocalDate, LocalDate> tempMap = new HashMap<>();
+        
+        synchronized (availableDates){
+            if (!availableDates.containsKey(dateStart)){
+                availableDates.put(dateStart, dateEnd);
+            }
+        }
+    }
+
+
+
+    public HashMap<LocalDate, LocalDate> getBookedDates() {
+        return bookedDates;
+    }
+
+
+
+
+
+    public boolean isAvailable(LocalDate dateFirst, LocalDate dateLast) {
+        LocalDate availDateFirst;
+        LocalDate availDateLast;
+
+        for (LocalDate date: availableDates.keySet()){
+            availDateFirst = date;
+            availDateLast = availableDates.get(date);
+
+            if ( (dateFirst == null) && (dateLast == null) )
+            {
+                return true;
+            } else if ( (dateLast == null)
+                    && (dateFirst.isAfter(availDateFirst) || dateFirst.isEqual(availDateFirst)) ){
+                return true;
+            }else if ( (dateFirst == null)
+                    && (dateLast.isBefore(availDateLast) || dateLast.isEqual(availDateLast)) ){
+                return true;
+            } else if ( (dateFirst != null) && (dateLast != null) && (dateFirst.isAfter(availDateFirst) || dateFirst.isEqual(availDateFirst))
+                    && (dateLast.isBefore(availDateLast) || dateLast.isEqual(availDateLast)) ){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+}
---
+++ src/Worker/WorkerFunctions.java
@@ -0,0 +1,152 @@
+package Worker;
+
+import Entities.*;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.*;
+import java.util.*;
+import java.time.LocalDate;
+import java.text.ParseException;
+
+
+public class WorkerFunctions {
+
+
+    public synchronized static boolean insert(Task task, HashMap<Integer, ArrayList<AccommodationRoom>> roomsMap){
+
+        if (roomsMap.get(task.getManagerID()) == null){
+            roomsMap.put(task.getManagerID(), new ArrayList<>());
+        }
+
+        AccommodationRoom room = new AccommodationRoom(task.getJson());
+        ArrayList<AccommodationRoom> list = roomsMap.get(task.getManagerID());
+        return(list.add(room));
+
+
+
+    }
+
+
+    public static void updateAvailableDates(Task task, HashMap<Integer, ArrayList<AccommodationRoom>> roomsMap){
+
+        ArrayList<AccommodationRoom> list = roomsMap.get(task.getManagerID());
+        for (AccommodationRoom room: list){
+            if (room.getName().equals(task.getRoomName())){
+                room.setAvailableDates(task.getDateFirst(), task.getDateLast());
+            }
+        }
+
+    }
+
+
+
+
+    public static ArrayList<AccommodationRoom> filterRooms(Task task, HashMap<Integer, ArrayList<AccommodationRoom>> roomsMap) throws java.text.ParseException {
+        ArrayList<AccommodationRoom> filteredRooms = new ArrayList<>();
+
+        synchronized (roomsMap) {
+            for (ArrayList<AccommodationRoom> rooms : roomsMap.values()) {
+                for (AccommodationRoom room : rooms) {
+
+                    // This block is synchronized
+                    if ((task.getAreaFilter().equals("null") || room.getArea().equals(task.getAreaFilter()))
+                            && (task.getCapacityFilter() == 0 || room.getCapacity() >= task.getCapacityFilter())
+                            && (task.getPriceFilter() == 0 || room.getPrice() <= task.getPriceFilter())
+                            && (task.getStarsFilter() == 0 || room.getStars() >= task.getStarsFilter())
+                            && (room.isAvailable(task.getDateFirst(), task.getDateLast()))
+                    ) {
+                        filteredRooms.add(room);
+                    }
+                }
+            }
+        }
+
+        return filteredRooms;
+    }
+
+
+    public static ArrayList<AccommodationRoom> showManagerRooms(Task task, HashMap<Integer, ArrayList<AccommodationRoom>> roomsMap){
+        System.out.println("list to return size: " + roomsMap.get(task.getManagerID()).size());
+        return roomsMap.get(task.getManagerID());
+    }
+
+    public static ArrayList<AccommodationRoom> showAllRooms(HashMap<Integer, ArrayList<AccommodationRoom>> roomsMap){
+        ArrayList<AccommodationRoom> allRooms = new ArrayList<>();
+        for (ArrayList<AccommodationRoom> managerRooms : roomsMap.values()) {
+            allRooms.addAll(managerRooms);
+        }
+        System.out.println(allRooms.size() + "----------");
+        return allRooms;
+    }
+
+
+    public static Socket connectWithReducer() {
+        try {
+            return new Socket("localhost", 1235);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public static void sendResultToReducer(Socket Reducer, int taskID, ArrayList<AccommodationRoom> result) {
+        try {
+            ObjectOutputStream objectOut = new ObjectOutputStream(Reducer.getOutputStream());
+            HashMap<Integer, ArrayList<AccommodationRoom>> map = new HashMap<>();
+            map.put(taskID, result);
+            System.out.println("TaskID: " + taskID);
+            objectOut.writeObject(map);
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public static boolean bookAroom(Task task, AccommodationRoom room) throws ParseException {
+        LocalDate availDateFirst;
+        LocalDate availDateLast;
+
+        synchronized (room) {
+            Set<LocalDate> availiableDatesSet = room.getAvailableDates().keySet();
+            for (LocalDate date :availiableDatesSet) {
+                availDateFirst = date;
+                availDateLast = room.getAvailableDates().get(date);
+
+                if (availDateFirst.isEqual(task.getDateFirst()) && availDateLast.isEqual(task.getDateLast())) {
+                    room.getBookedDates().put(task.getDateFirst(),task.getDateLast());
+                    room.getAvailableDates().remove(availDateFirst);
+                    return true;
+                }
+                else if (availDateFirst.isEqual(task.getDateFirst()) && task.getDateLast().isBefore(availDateLast)) {
+                    room.getBookedDates().put(task.getDateFirst(),task.getDateLast());
+                    room.getAvailableDates().remove(availDateFirst);
+                    room.getAvailableDates().put(task.getDateLast().plusDays(1), availDateLast);
+                    return true;
+
+                }
+                else if (task.getDateFirst().isAfter(availDateFirst) && availDateLast.isEqual(task.getDateLast())) {
+                    room.getBookedDates().put(task.getDateFirst(),task.getDateLast());
+                    room.getAvailableDates().remove(availDateFirst);
+                    room.getAvailableDates().put(availDateFirst, task.getDateFirst().minusDays(1));
+                    return true;
+                }
+                else if (task.getDateFirst().isAfter(availDateFirst) && task.getDateLast().isBefore(availDateLast)) {
+                    room.getBookedDates().put(task.getDateFirst(),task.getDateLast());
+                    room.getAvailableDates().remove(availDateFirst);
+                    room.getAvailableDates().put(availDateFirst, task.getDateFirst().minusDays(1));
+                    room.getAvailableDates().put(task.getDateLast().plusDays(1), availDateLast);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+
+}
